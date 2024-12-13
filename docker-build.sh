b67bebd756747f612ad79783aa4a62e7e79b6b83
Ivan Franchin <ivan.franchin@takeaway.com>
2024-12-13 10:55:16 -0300

    Refactor the way docker images are built

--- docker-build.sh
+++ docker-build.sh
@@ -4,6 +4,26 @@ source scripts/my-functions.sh
 
 check_script_input_parameter $1
 
+DOCKER_IMAGE_PREFIX="ivanfranchin"
+APP_VERSION="1.0.0"
+
+MOVIE_SERVICE_APP_NAME="movie-service"
+RESTAURANT_SERVICE_APP_NAME="restaurant-service"
+STUDENT_SERVICE_APP_NAME="student-service"
+BOOK_SERVICE_APP_NAME="book-service"
+
+MOVIE_SERVICE_PROJECT_NAME="spring-vault-approle-mysql/${MOVIE_SERVICE_APP_NAME}"
+RESTAURANT_SERVICE_PROJECT_NAME="spring-vault-approle-multi-datasources-mysql/${RESTAURANT_SERVICE_APP_NAME}"
+STUDENT_SERVICE_PROJECT_NAME="spring-cloud-vault-approle-mysql/${STUDENT_SERVICE_APP_NAME}"
+BOOK_SERVICE_PROJECT_NAME="spring-cloud-vault-approle-cassandra/${BOOK_SERVICE_APP_NAME}"
+
+MOVIE_SERVICE_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${MOVIE_SERVICE_APP_NAME}:${APP_VERSION}"
+RESTAURANT_SERVICE_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${RESTAURANT_SERVICE_APP_NAME}:${APP_VERSION}"
+STUDENT_SERVICE_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${STUDENT_SERVICE_APP_NAME}:${APP_VERSION}"
+BOOK_SERVICE_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${BOOK_SERVICE_APP_NAME}:${APP_VERSION}"
+
+SKIP_TESTS="true"
+
 if [ "$1" = "spring-vault-approle-mysql" ] ||
    [ "$1" = "all" ];
 then
@@ -13,7 +33,10 @@ then
   echo "spring-vault-approle-mysql"
   echo "--------------------------"
 
-  ./mvnw clean compile jib:dockerBuild --projects spring-vault-approle-mysql/movie-service
+  ./mvnw clean compile jib:dockerBuild \
+    --projects "$MOVIE_SERVICE_PROJECT_NAME" \
+    -DskipTests="$SKIP_TESTS" \
+    -Dimage="$MOVIE_SERVICE_DOCKER_IMAGE_NAME"
 
 fi
 
@@ -26,7 +49,10 @@ then
   echo "spring-vault-approle-multi-datasources-mysql"
   echo "--------------------------------------------"
 
-  ./mvnw clean compile jib:dockerBuild --projects spring-vault-approle-multi-datasources-mysql/restaurant-service
+  ./mvnw clean compile jib:dockerBuild \
+    --projects "$RESTAURANT_SERVICE_PROJECT_NAME" \
+    -DskipTests="$SKIP_TESTS" \
+    -Dimage="$RESTAURANT_SERVICE_DOCKER_IMAGE_NAME"
 
 fi
 
@@ -39,7 +65,10 @@ then
   echo "spring-cloud-vault-approle-mysql"
   echo "--------------------------------"
 
-  ./mvnw clean compile jib:dockerBuild --projects spring-cloud-vault-approle-mysql/student-service
+  ./mvnw clean compile jib:dockerBuild \
+    --projects "$STUDENT_SERVICE_PROJECT_NAME" \
+    -DskipTests="$SKIP_TESTS" \
+    -Dimage="$STUDENT_SERVICE_DOCKER_IMAGE_NAME"
 
 fi
 
@@ -52,6 +81,9 @@ then
   echo "spring-cloud-vault-approle-cassandra"
   echo "------------------------------------"
 
-  ./mvnw clean compile jib:dockerBuild --projects spring-cloud-vault-approle-cassandra/book-service
+  ./mvnw clean compile jib:dockerBuild \
+    --projects "$BOOK_SERVICE_PROJECT_NAME" \
+    -DskipTests="$SKIP_TESTS" \
+    -Dimage="$BOOK_SERVICE_DOCKER_IMAGE_NAME"
 
 fi
